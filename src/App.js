22a8bea9c48ebeb03033b9da334cde1e2cf81e42
Nizar Can <nizarcan@yahoo.com>
2021-05-13 21:04:43 +0300

    login functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
+import React, {useEffect, useState} from "react";
 import Login from "./Login";
+import './App.css';
+import { tokenFromURL } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = tokenFromURL();
+    
+    window.location.hash = "";
+
+    const accessToken = hash.access_token;
+
+    if (accessToken) {
+      setToken(accessToken);
+    }
+  }, []);
+
   return (
     <div className="app">  
-    {/* Login */}
-    <Login/>
-    
+    {
+      token ? (
+        <h1>Logged in.</h1>
+      ) : (
+        <Login/>
+      )
+    }
+
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Login.css';
 import spotifyLogo from "./assets/spotify-logo.jpg";
+import {loginURL} from "./spotify";
 
 function Login() {
     return (
@@ -8,7 +9,7 @@ function Login() {
             {/* Logo */}
             <img src={spotifyLogo} alt=""/>
             {/* Login Button */}
-            <a>LOGIN WITH SPOTIFY</a>
+            <a href={loginURL}>LOGIN WITH SPOTIFY</a>
 
         </div>
     )
